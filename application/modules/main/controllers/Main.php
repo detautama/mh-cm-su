dfa22b4b78ab85fef0efcb9a2ab0d7ea25df81e7
Buddy <buddyy93@gmail.com>
2018-03-04 13:23:58 +0800

    add marketplace login page

--- application/modules/main/controllers/Main.php
+++ application/modules/main/controllers/Main.php
@@ -16,4 +16,14 @@ class Main extends MX_Controller {
         $this->template->content->view('main/login');
         $this->template->publish();
     }
+
+    public function marketplace()
+    {
+        $this->template->set_template('frontend/template');
+        $this->template->title = 'MarketHub Channel Manager';
+        $this->template->stylesheet->add(base_url() . "public/css/index.css");
+
+        $this->template->content->view('main/marketplace-login');
+        $this->template->publish();
+    }
 }
